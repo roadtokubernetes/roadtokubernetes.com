f3886525e8b9363d53d131a8bbc92e42e266eb34
Justin Mitchel <j@teamcfe.com>
2022-10-12 23:31:11 -0500

    health checks

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -21,7 +21,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("admin/", admin.site.urls),
-    path('<path:resource>', TemplateView.as_view(template_name='coming-soon.html')),
     path("healthz", views.index),
+    path("admin/", admin.site.urls),
+    path("<path:resource>", TemplateView.as_view(template_name="coming-soon.html")),
 ]
