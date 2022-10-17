4dab1be68b08eb3e46f60b42297d886efc90d4af
Justin Mitchel <j@teamcfe.com>
2022-10-17 12:49:20 -0500

    Added FIXTURES_DIR

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+FIXTURES_DIR =  BASE_DIR / "fixtures"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
