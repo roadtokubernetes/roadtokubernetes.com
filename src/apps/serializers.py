fafeefee671970077c28a18f97a0b982f787de76
Justin Mitchel <j@teamcfe.com>
2022-11-03 08:55:37 -0500

    Updated ui

--- src/apps/serializers.py
+++ src/apps/serializers.py
@@ -6,17 +6,36 @@ from .models import App
 class AppRawSerializer(serializers.ModelSerializer):
     project_id = serializers.CharField(source="project.project_id", read_only=True)
     allow_internet_traffic = serializers.BooleanField()
+    environment_variables = serializers.SerializerMethodField(read_only=True)
+    secrets = serializers.SerializerMethodField(read_only=True)
+    domain_names = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = App
-        fields = [
-            "app_id",
-            "project_id",
-            "name",
-            "container",
-            "container_port",
-            "image_pull_policy",
-            "allow_internet_traffic",
-            "tls_secret_name",
-            "custom_domain_names",
-        ]
+        fields = sorted(
+            [
+                "app_id",
+                "project_id",
+                "name",
+                "container",
+                "container_port",
+                "image_pull_policy",
+                "allow_internet_traffic",
+                "tls_secret_name",
+                "domain_names",
+                "environment_variables",
+                "secrets",
+            ]
+        )
+
+    def get_environment_variables(self, obj):
+        return obj.get_container_environment_variables()
+
+    def get_secrets(self, obj):
+        secrets, exists = obj.get_secret_variables()
+        if not exists:
+            return None
+        return secrets
+
+    def get_domain_names(self, obj):
+        return obj.get_domain_names()
